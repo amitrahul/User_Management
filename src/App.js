6eb4fc8d36ac033f2d4728f363450a60c1bff904
amitrahul <raj.rahul686@gmail.com>
2023-10-07 21:39:53 +0530

    Implemented Add,delete and edit user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import DashBoard from "./components/Dashboard/Dashboard";
 import NavBar from "./components/NavBar/NavBar";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import NewUser from "./components/NewUser/NewUser";
+import { createBrowserRouter, RouterProvider , Outlet } from "react-router-dom";
+import Errors from "./components/Error/Errors";
 
 const App = () => {
   return (
     <>
       <NavBar />
-        <DashBoard />
-    
+      <Outlet />
     </>
   );
 };
 
-export default App;
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element:  <DashBoard />,
+      },
+      {
+        path: "/user",
+        element: <NewUser />,
+      },
+    ],
+    errorElement: <Errors />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
+// root.render(<App />)
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 const NavBar = () => {
 
     return(
-        <div className={styles.nav}>
-            User Management
+        <div className={styles.navWrapper}>
+         <div className={styles.nav}>
+            <Link to="/"> User Management</Link>
+        </div>
+        <div className={styles.user} >
+            <Link to="/user"> Create User</Link>
+        
+        </div>
         </div>
     )
 }
